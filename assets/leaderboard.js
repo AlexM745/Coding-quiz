7adbbe2ae75dcf108d79ce21b141b06247fde47e
Alexis Merino <alexismerino49@gmail.com>
2024-02-20 23:59:17 -0600

    fixed bugs on script.js and finished leaderboard.js

--- assets/leaderboard.js
+++ assets/leaderboard.js
@@ -1,5 +1,26 @@
 
+// javascript for leaderbaord
+
+
 // document elements 
-var scorebtn = document.querySelector("#view-scores")
+var scorebtn = document.querySelector("#view-scores");
 
 // to store past scores on leaderboard
+function printscores() {
+    let highscores =
+        JSON.parse(window.localStorage.getItem("highscores")) || [];highscores.sort(function (a, b) {return b.score - a.score;});
+    highscores.forEach(function ( score) {
+        let liTag = document.createElement("li");
+        liTag.textContent =score.name +" - " + score.score;
+        let olEl = document.getElementById("highscores");
+        olEl.appendChild(liTag);});
+}
+
+// actives the clear button when user wants to clear scores
+function clearHighscores() {
+    window.localStorage.removeItem("highscores");window.location.reload();
+}
+
+document.querySelector("#reset").addEventListener("click",clearHighscores,printscores);
+
+printscores();
--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,45 @@
+/// main javasript for quiz
+
+
 // document elements 
 var scoresEL = document.querySelector("#view-scores");
 var timerEL = document.querySelector("#timer");
 var startbtn = document.querySelector("#start-button");
 var questionsEL = document.querySelector("#questions");
-let choices =document.querySelector("#answeropts");
+var choices =document.querySelector("#answeropts");
 var nameEL = document.querySelector("#name");
 var submitbtn = document.querySelector("#submit-score");
 var feedbackEl= document.querySelector("#feedback");
 var restartbtn = document.querySelector("#restart");
 
+
 // array with question prompts, options and answer
 var questprompts = [
-    //1
+    //0
     {   
         prompt:"Inside which HTML do we put the Javasript?",
         options:["<javascript>", "<js>", "<script>","<link>"],
         answer:"<script>"
     },
-    //2
+    //1
     {
         prompt:"How do you write an alert?",
         options:["msg();", "prompt();", "alertbox();","alert();"],
         answer:"alert();"
     },
-    //3
+    //2
     {
         prompt:"How do you write a function in Javasript?",
         options:["function:name()", "function name()", "function = name()","function[name]()"],
         answer:"function name()"
     },
-    //4
+    //3
     {
         prompt:"Which of the following are Data types?",
         options:["Booleans", "Numbers", "Strings","All of the Above"],
         answer:"All of the Above"
     },
-    //5
+    //6
     {
     
         prompt:"What is event delagation ?",
@@ -44,41 +48,43 @@ var questprompts = [
     }
     ];
 
-    var time = questprompts.length * 12;
-    var timerid;
+var time = questprompts.length * 10;// add 50 secs to the timer
+var questnumb = 0;// used to access the array
+var timerid;
     
 // Start button and bring up question prompts and hides frontpage
 function quizstart() {
     timerid = setInterval(timeend, 1000);//sets the timer interval
     timerEL.textContent= time;// adds value to time.
-    var startpage = document.getElementById("start-page");
+    var startpage = document.querySelector("#start-page");
     startpage.setAttribute("class","hide");
-    questionsEL.remove("class");
+    questionsEL.setAttribute("class","visible");
     showquest();
 }
 
 
 // function to get questions from questprompt object
 function showquest() {
-    var questtext = questprompts[0];
-	let promptEl = document.getElementById("questions-prompts");
+    var questtext = questprompts[questnumb];// get the prompts from array
+	var promptEl = document.querySelector("#questions-prompts");
 	promptEl.textContent = questtext.prompt;
 	choices.innerHTML = "";
+
 	questtext.options.forEach(
 		function (option, i) {
-			let choiceBtn =
+			var choiceBtn =
 				document.createElement("button");
 			choiceBtn.setAttribute("value",option);
 			choiceBtn.textContent =
 				i + 1 + ". " + option;
-			choiceBtn.onclick = optionclick;
+			choiceBtn.addEventListener('click', optionclick);
 			choices.appendChild(choiceBtn);
 		}
 	);
 }
 // when presssing the answer options
 function optionclick() {
-	if (this.value !==questprompts[0].answer) {
+	if (this.value !==questprompts[questnumb].answer) {
 		time -= 10;
 		if (time < 0) {time = 0;}
 		timerEL.textContent = time;
@@ -95,9 +101,9 @@ function optionclick() {
 			"class",
 			"feedback hide"
 		);
-	}, 2000);
-	questprompts[0]++;
-	if (questprompts[0] === questprompts.length) {quizEnd();} 
+	}, 1000);
+	questnumb++;
+	if (questnumb === questprompts.length) {quizfinished();} 
     else {
 		showquest();
 	}
@@ -108,7 +114,7 @@ function timeend(){
     time--; // timer decrements
     timerEL.textContent = time;// adds the 60 to the timer
     if (time <= 0) { 
-        end();// if the timer is equal or greater than to zero call end function.
+        quizfinished();// if the timer is equal or greater than to zero call quiz finished function.
     } 
 }
 // quiz ends when timer is down to 0 
@@ -116,7 +122,7 @@ function quizfinished (){
     clearInterval(timerid);// clears the timer 
     var endquiz = document.querySelector("#end");// selects the end element from HTML
     endquiz.removeAttribute("class");//removes the hide classs
-    var finalscore = document.getElementById("#final-score"); //selects the final score element by the id 
+    var finalscore = document.querySelector("#final-score"); //selects the final score element by the id 
     finalscore.textContent = time;// the score is the time left.
     questionsEL.setAttribute("class", "hide");// hides the questions
 }
@@ -126,9 +132,12 @@ function saveinfo() {
     var name = nameEL.value.trim()// gets the name input value an trims
     if (name !== "") {
         var highscore = JSON.parse(window.localStorage.getItem("highscores")) || [];
-        var newscore = {score: time,name: name,};
+        var newscore = {
+            score: time,
+            name: name,
+        };
     }
-    scoresEL.push(newscore);
+    highscore.push(newscore);
     window.localStorage.setItem("highscores", JSON.stringify(highscore));
 }
 
