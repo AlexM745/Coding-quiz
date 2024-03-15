8f5eb383df289ab797553de1279a7579b964c084
Alexis Merino <alexismerino49@gmail.com>
2024-03-14 20:39:27 -0500

    question and answers with buttons code works

--- assets/script.js
+++ assets/script.js
@@ -2,60 +2,73 @@
 let scores = document.querySelector("#view-scores");
 let timer = document.querySelector("#timer");
 let startbtn = document.querySelector("#start-button");
-let questions= document.querySelector("#questions");
-let choices =document.querySelector("#answeropts");
+let questions = document.querySelector("#questions");
+let answerOpts = document.querySelector("#answeropts");
 let name = document.querySelector("#name");
 let submitbtn = document.querySelector("#submit-score");
-let feedback= document.querySelector("#feedback");
+let feedback = document.querySelector("#feedback");
 let restartbtn = document.querySelector("#restart");
 let startscreen = document.querySelector("#start-page")
-let questionIndex = 0
+let questionWords = document.querySelector("#questions-prompts");
+let questionIndex = 0;
+let timerStart = '';
 
 
-// array with question prompts, options and answer
+// array with question prompts, options and answer as and object
 const questprompts = [
     //0
-    {   
-        prompt:"Inside which HTML do we put the Javasript?",
-        options:["<javascript>", "<js>", "<script>","<link>"],
-        answer:"<script>"
+    {
+        prompt: "Inside which HTML do we put the Javasript?",
+        options: ["<javascript>", "<js>", "<script>", "<link>"],
+        answer: "<script>"
     },
     //1
     {
-        prompt:"How do you write an alert?",
-        options:["msg();", "prompt();", "alertbox();","alert();"],
-        answer:"alert();"
+        prompt: "How do you write an alert?",
+        options: ["msg();", "prompt();", "alertbox();", "alert();"],
+        answer: "alert();"
     },
     //2
     {
-        prompt:"How do you write a function in Javasript?",
-        options:["function:name()", "function name()", "function = name()","function[name]()"],
-        answer:"function name()"
+        prompt: "How do you write a function in Javasript?",
+        options: ["function:name()", "function name()", "function = name()", "function[name]()"],
+        answer: "function name()"
     },
     //3
     {
-        prompt:"Which of the following are Data types?",
-        options:["Booleans", "Numbers", "Strings","All of the Above"],
-        answer:"All of the Above"
+        prompt: "Which of the following are Data types?",
+        options: ["Booleans", "Numbers", "Strings", "All of the Above"],
+        answer: "All of the Above"
     },
     //6
     {
-    
-        prompt:"What is event delagation ?",
-        options:["Apply event to child via its parent", "Apply event to parent via its child", "Apply event to button via HTML"],
-        answer:"function name()"
+
+        prompt: "What is event delagation ?",
+        options: ["Apply event to child via its parent", "Apply event to parent via its child", "Apply event to button via HTML"],
+        answer: "function name()"
     }
-    ];
+];
 
 // start quiz after start button is clicked
-startbtn.addEventListener('click', function(){
-    time = setInterval(1000)
-questions.classList.remove("hide");
-startscreen.classList.add("hide");
- showquestions();
+startbtn.addEventListener('click', function () {
+    timerStart = setInterval(60*1000);
+    questions.classList.remove("hide");
+    startscreen.classList.add("hide");
+    showquestions();
 });
 
+
+// fucntion to show the questions and the answer choices
 function showquestions() {
     let prompts = questprompts[questionIndex];
-    console.log(prompts);
+    questionWords.textContent = prompts.prompt;
+    answerOpts.innerHTML = "";
+    // for loop to get the answer choices
+    for (var i = 0; i < prompts.options.length; i++) {
+        let answerBtn = document.createElement("button");
+        let choices = prompts.options[i];        
+        answerBtn.setAttribute("value", choices);
+        answerBtn.textContent = i + 1 + "." + choices;
+        answerOpts.appendChild(answerBtn);
+    }
 }
